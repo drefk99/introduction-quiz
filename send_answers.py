c7de124d87efdee59d2bf3b87e3fe5b741d78d48
drefk99 <drefk94@gmail.com>
2017-03-28 09:57:14 -0600

    Super forma final

--- send_answers.py
+++ send_answers.py
@@ -1,14 +1,21 @@
 import json
 import pandas as pd
 import requests
+import sys
 
+##Cargar archivo json
 with open('introduction_quiz.json') as data_file:
 
-	data=json.load(data_file)
-
-df = pd.DataFrame(data)
-
-url='localhost'
-r=requests.post(url, params=data_file)
-
+	datajs=json.load(data_file)
+##Variable para obetener argumentos del usuario
+args=sys.argv
+##URL que ingresa usuario
+url=args[1]
+##ejemplo 'http://localhost:9200/examen/jorge/intro?pretty=true'
+##Posteo del json en la dirección 
+r=requests.post(url, data=json.dumps(datajs))
+##Descarga información de la URL despues del posteo
+impre=requests.get(url)
+df = impre.text
+##Impresión en la terminal
 print(df)
